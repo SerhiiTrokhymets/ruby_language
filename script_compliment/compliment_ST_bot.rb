e78feac00182b344526b6068658b2449cb57f81e
Serhii Trokhymets <serheyfly4410@gmail.com>
2020-12-14 22:12:58 +0200

    correct

--- script_compliment/compliment_ST_bot.rb
+++ script_compliment/compliment_ST_bot.rb
@@ -6,7 +6,7 @@ require 'dotenv/load'
 
 Dotenv.load('TOKEN.env')
 COMANDS = {
-  start: '/start', stop: '/stop', help: '/help', complimetn: '/comp'
+  start: '/start', stop: '/stop', help: '/help', compliment: '/comp'
 }.freeze
 puts COMANDS[:complimetn]
 compliment_girl(URL)
@@ -15,7 +15,7 @@ compliment_girl(URL3)
 text_help = <<-TEXT
 Для начала общения #{COMANDS[:start]}
 Для завершения #{COMANDS[:stop]}
-Получить комплимент #{COMANDS[:complimetn]}
+Получить комплимент #{COMANDS[:compliment]}
 TEXT
 
 Telegram::Bot::Client.run(ENV['TOKEN']) do |bot|
@@ -25,13 +25,12 @@ Telegram::Bot::Client.run(ENV['TOKEN']) do |bot|
       bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
     when COMANDS[:stop]
       bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
-    when '/help'
-    when COMANDS[:complimetn]
+    when COMANDS[:compliment]
       bot.api.send_message(chat_id: message.chat.id, text: "#{ARRAY_TEXT_GIRL.sample}")
     when COMANDS[:help]
       bot.api.send_message(chat_id: message.chat.id, text: text_help)
     else
-      bot.api.send_message(chat_id: message.chat.id, text: "Нет такой команды. Введите /help для прсомотра доступных команд")
+      bot.api.send_message(chat_id: message.chat.id, text: "Нет такой команды. Введите /help для просмотра доступных команд")
     end
   end
 end
